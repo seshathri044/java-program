8a98eeaef7d8fe65f855032e17dfaba12472dbf2
seshathri044 <mseshathri2004@gmail.com>
2025-05-30 22:42:16 +0530

    add

---
+++ loop.java
@@ -0,0 +1,12 @@
+public class loop {
+    public static void main(String [] args){
+        int n=3;
+        for(int i=0;i<n;i++){
+            System.out.println(i);
+        }
+        while(n>=0){
+            System.out.println(n);
+            n--;
+        }
+    }
+}
