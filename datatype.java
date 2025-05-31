6e8e759ab8640fe33766d42389b33bcbade56029
seshathri044 <mseshathri2004@gmail.com>
2025-05-31 20:09:25 +0530

    add

---
+++ datatype.java
@@ -0,0 +1,14 @@
+public class datatype {
+    public static void main(String [] args){
+        int a = 5;
+        double b = 10.3;
+        String c = "seshathri";
+        long d = 9223372036854775807L;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        double e = a ;
+        System.out.println(e);
+    }
+}
