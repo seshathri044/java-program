80d658f29f6218a108d918ef669bee7f396dd6fd
seshathri044 <mseshathri2004@gmail.com>
2025-06-03 00:45:41 +0530

    add

---
+++ methods.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class methods {
+    public static int int_val(Scanner sc){
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println(c);
+        return c;
+    }
+    void String_val(Scanner sc){
+        String sr = sc.next();
+        System.out.println(sr);
+    }
+    public static void main(String [] args){
+        System.out.println("Enter the value");
+        Scanner sc = new Scanner(System.in);
+        methods nw = new methods();
+        methods.int_val(sc);
+        nw.String_val(sc);
+    }
+}
